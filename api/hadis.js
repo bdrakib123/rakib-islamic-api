bfda8b43b5c7c0eb88b206578fb8ddd2323ef7f3
Rakib <spyer.rakib@gmail.com>
2026-03-04 10:24:54 +0600

    Update hadis.js

--- api/hadis.js
+++ api/hadis.js
@@ -1,23 +1,33 @@
 const express = require("express");
 const router = express.Router();
-const hadis = require("../data/hadis.json");
+const quran = require("../data/hadis.json");
 
 // Random Hadis
 router.get("/random", (req, res) => {
   const random = hadis[Math.floor(Math.random() * hadis.length)];
-  res.json({ status: true, data: random });
+  res.json({
+    status: true,
+    data: random
+  });
 });
 
 // By ID
 router.get("/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  const result = hadis.find(h => h.id === id);
+
+  const result = hadis.find(q => q.id === id);
 
   if (!result) {
-    return res.json({ status: false, message: "Hadis not found" });
+    return res.json({
+      status: false,
+      message: "Hadis not found"
+    });
   }
 
-  res.json({ status: true, data: result });
+  res.json({
+    status: true,
+    data: result
+  });
 });
 
 module.exports = router;
